beccd50a62c19197f37def2432565909748b4262
sand0rka <115060928+sand0rka@users.noreply.github.com>
2023-05-15 20:08:20 +0300

    add main.py and make private fields public

--- Trolleybus.py
+++ Trolleybus.py
@@ -29,91 +29,41 @@ class Trolleybus:
     default_trolleybus = None
 
     def __init__(self, route_number=0, current_stop="", max_speed=0.0, capacity=0, passengers=0):
-        self.__id = 100
-        self.__route_number = route_number
-        self.__current_stop = current_stop
-        self.__max_speed = max_speed
-        self.__capacity = capacity
-        self.__passengers = passengers
+        self.id = 100
+        self.route_number = route_number
+        self.current_stop = current_stop
+        self.max_speed = max_speed
+        self.capacity = capacity
+        self.passengers = passengers
+
+        def __cmp__ (self, other):
+          return self.route_number == other.route_number and self.current_stop == other.current_stop and \
+                self.max_speed == other.max_speed and self.capacity == other.capacity and\
+                self.passengers == other.passengers
+
+    
 
     def stop(self):
         current_speed = 0.0
 
     def start(self):
-        current_speed = 0.0
+        current_speed = 20.0
 
     def add_passenger(self):
-        if self.__passengers < self.__capacity:
-            self.__passengers += 1
+        if self.passengers < self.capacity:
+            self.passengers += 1
 
     def remove_passenger(self):
-        if self.__passengers > 0:
-            self.__passengers -= 1
+        if self.passengers > 0:
+            self.passengers -= 1
 
     def __str__(self):
-        return f"Trolleybus(id={self.__id}, routeNumber={self.__route_number}, currentStop='{self.__current_stop}', " \
-               f"maxSpeed={self.__max_speed}, capacity={self.__capacity}, passengers={self.__passengers})"
+        return f"Trolleybus(id={self.id}, routeNumber={self.route_number}, currentStop='{self.current_stop}', " \
+               f"maxSpeed={self.max_speed}, capacity={self.capacity}, passengers={self.passengers})"
 
-    @property
-    def id(self):
-        return self.__id
-
-    @id.setter
-    def id(self, value):
-        self.__id = value
-
-    @property
-    def route_number(self):
-        return self.__route_number
-
-    @route_number.setter
-    def route_number(self, value):
-        self.__route_number = value
-
-    @property
-    def current_stop(self):
-        return self.__current_stop
-
-    @current_stop.setter
-    def current_stop(self, value):
-        self.__current_stop = value
-
-    @property
-    def max_speed(self):
-        return self.__max_speed
-
-    @max_speed.setter
-    def max_speed(self, value):
-        self.__max_speed = value
-
-    @property
-    def capacity(self):
-        return self.__capacity
-
-    @capacity.setter
-    def capacity(self, value):
-        self.__capacity = value
-
-    @property
-    def passengers(self):
-        return self.__passengers
-
-    @passengers.setter
-    def passengers(self, value):
-        self.__passengers = value
 
     @staticmethod
     def get_instance():
         if Trolleybus.default_trolleybus is None:
             Trolleybus.default_trolleybus = Trolleybus()
         return Trolleybus.default_trolleybus
-
-
-if __name__ == '__main__':
-    trolleybuses = [Trolleybus(),
-                    Trolleybus(122, "Sknylivok", 70.0, 50, 42),
-                    Trolleybus.get_instance(),
-                    Trolleybus.get_instance()]
-
-    for trolleybus in trolleybuses:
-        print(trolleybus)
